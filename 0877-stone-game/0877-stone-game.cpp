2494b624cc5d11da5839444aab14eba15c6f1bd6
Shweta Jha <84969672+SHWETA2JHA@users.noreply.github.com>
2026-08-09 18:15:06 +0530

    Time: 11 ms (22.23%), Space: 19.7 MB (14.83%) - LeetHub

--- 0877-stone-game/0877-stone-game.cpp
+++ 0877-stone-game/0877-stone-game.cpp
@@ -1,6 +1,20 @@
 class Solution {
 public:
     bool stoneGame(vector<int>& piles) {
-        return 1;
+        int n=piles.size();
+        vector<vector<int>>dp(n,vector<int>(n,0));
+
+        for(int i=0; i<n; i++)dp[i][i]=piles[i];
+
+        for(int len=2; len<=n; len++){
+            for(int j=0; j+len-1<n; j++){
+                int i=j+len-1;
+                int takel=piles[j]+dp[j+1][i];
+                int taker=piles[i]+dp[j][i-1];
+                dp[j][i]=max(takel, taker);
+            }
+        }
+
+        return dp[0][n-1]>0;
     }
 };
